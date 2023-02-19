8261ad43684a02ee6e1a1e2643abda0470574aa1
Ahmet Karakaya <karakayahmet00@gmail.com>
2023-02-19 09:52:44 +0300

    first commit

---
+++ bolum1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Data.SqlClient;
+using MySql.Data.MySqlClient;
+
+namespace bolum1;
+class Program
+{
+    static void Main(string[] args)
+    {
+        getSqlConnection();
+        getMySqlConnection();
+    }
+
+    static void getSqlConnection(){
+        string sqlconnection = @"Data Source=\Sqlexpress; Initial Catalog=Northwind; Integrated Security=SSPI;";
+
+        using(var connection = new SqlConnection(sqlconnection)){
+
+            try{
+                connection.Open();
+                Console.WriteLine("Bağlantı Sağlandı");
+            }
+
+            catch(Exception e){
+                Console.WriteLine(e.Message);
+            }
+
+            finally{
+                connection.Close();
+            }
+        }
+    }
+
+    static void getMySqlConnection(){
+        string mysqlconnection = @"server=localhost; port=3306; database=Northwind; user=root; password=12345;";
+
+        using(var connection = new MySqlConnection(mysqlconnection) ){
+            try{
+                connection.Open();
+                Console.WriteLine("Bağlantı sağlandı.");
+            }
+
+            catch(Exception e){
+                Console.WriteLine(e.Message);
+            }
+
+            finally{
+                connection.Close();
+            }
+        }
+    }
+}
