e3d49714c898e67d859f3e8fbca28f9c4ea803b8
Ahmet Karakaya <karakayahmet00@gmail.com>
2023-02-19 10:10:49 +0300

    veritabanından ürün bilgileri alındı.

---
+++ bolum2/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace bolum2;
+using MySql.Data.MySqlClient;
+using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        getAllProducts();
+    }
+
+    static void getAllProducts(){
+        using(var con = GetMySqlConnection()){
+            try{
+                con.Open();
+                Console.WriteLine("Bağlantı Sağlandı.");
+
+                string sql = "select * from products";
+
+                MySqlCommand command = new MySqlCommand(sql, con);
+
+                MySqlDataReader reader = command.ExecuteReader();
+
+                while(reader.Read()){
+                    Console.WriteLine($"name:{reader[3]} , price: {reader[6]}");
+                } 
+                
+                reader.Close();
+            }
+
+            catch(Exception e){
+                Console.WriteLine(e.Message);
+            }
+
+            finally{
+                con.Close();
+            }
+        }
+    }
+
+    static MySqlConnection GetMySqlConnection(){
+        string connection = @"server=localhost; port=3306; database=Northwind; user=root; password=mysql123";
+        return new MySqlConnection(connection);
+    }
+}
