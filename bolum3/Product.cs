a0545ef1350783ec52d3b42e8c24e4b909bf0cef
Ahmet Karakaya <karakayahmet00@gmail.com>
2023-02-19 16:44:40 +0300

    bölüm 3 tamamlandı.

---
+++ bolum3/Product.cs
@@ -0,0 +1,8 @@
+namespace bolum3;
+
+public class Product{
+    public int ProductId { get; set; }
+    public string Name { get; set; }
+
+    public double Price { get; set; }
+}
--- bolum3/Program.cs
+++ bolum3/Program.cs
@@ -1,8 +1,61 @@
 ﻿namespace bolum3;
+using MySql.Data.MySqlClient;
+using System.Collections.Generic;
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var products = getAllProducts();
+
+        foreach (var item in products)
+        {
+            Console.WriteLine($"Product Id : {item.ProductId}, Product Name : {item.Name}, Product Price : {item.Price}");
+        }
+    }
+
+    static List<Product> getAllProducts(){
+    
+        List<Product> products = null;
+        using(var con = mySqlConnection()){
+            try{
+                con.Open();
+                Console.WriteLine("Bağlantı sağlandı.");
+
+                string sql = "select * from products";
+
+                MySqlCommand command = new MySqlCommand(sql, con);
+
+                MySqlDataReader reader = command.ExecuteReader();
+
+                products = new List<Product>();
+
+                while(reader.Read()){
+                    products.Add(new Product{
+                        ProductId = int.Parse(reader["id"].ToString()),
+                        Name = reader["product_name"].ToString(),
+                        Price = double.Parse(reader["list_price"].ToString())
+                    });
+                }
+
+                reader.Close();
+            }
+
+            catch(Exception e){
+                Console.WriteLine(e.Message);
+            }
+
+            finally{
+                con.Close();
+            }
+        }
+
+        return products;
+    }
+
+    static MySqlConnection mySqlConnection(){
+        string connection = @"server=localhost; port=3306; database=Northwind; user=root; password=mysql123;";
+
+        return new MySqlConnection(connection);
     }
 }
