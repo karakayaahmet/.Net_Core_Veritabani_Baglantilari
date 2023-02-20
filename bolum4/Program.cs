0a64536d459e26d9f76420df8cd73e23622861ad
Ahmet Karakaya <karakayahmet00@gmail.com>
2023-02-20 20:58:14 +0300

    kriterlere göre ürün filitreleme tamamlandı.

--- bolum4/Program.cs
+++ bolum4/Program.cs
@@ -7,6 +7,8 @@ interface IProductDal{
 
     Products GetProductById(int id);
 
+    List<Products> Find(string productName);
+
     void Create(Products p);
 
     void Update(Products p);
@@ -33,7 +35,46 @@ class MySqlProductDal : IProductDal
 
     public Products GetProductById(int id)
     {
-        throw new NotImplementedException();
+        Products product = null;
+
+        using(var con = getMySqlConnection()){
+            try{
+                con.Open();
+                Console.WriteLine("Bağlantı Sağlandı.");
+
+                string sql = "select * from products where id = @id";
+
+                MySqlCommand command = new MySqlCommand(sql, con);
+
+                command.Parameters.Add("@id", MySqlDbType.Int32).Value=id;
+
+                MySqlDataReader reader = command.ExecuteReader();
+
+                reader.Read();
+
+                if (reader.HasRows){
+                    product = new Products(){
+                        ProductId = int.Parse(reader["id"].ToString()),
+                        Name = reader["product_name"].ToString(),
+                        Price = double.Parse(reader["list_price"].ToString())
+                    };
+
+                }
+
+                reader.Close();
+            }
+
+            catch(Exception e){
+                Console.WriteLine(e.Message);
+            }
+
+            finally{
+                con.Close();
+            }
+        }
+
+        return product;
+
     }
 
     public List<Products> GetProducts()
@@ -79,6 +120,48 @@ class MySqlProductDal : IProductDal
     {
         
     }
+
+    public List<Products> Find(string productName)
+    {
+        List<Products> products = null;
+
+        using(var con = getMySqlConnection()){
+            try{
+                con.Open();
+                Console.WriteLine("Bağlantı Sağlandı.");
+
+                string sql = "select * from products where product_name like @productName";
+
+                MySqlCommand command = new MySqlCommand(sql, con);
+
+                command.Parameters.Add("@productName", MySqlDbType.String).Value = "%"+productName+"%";
+
+                MySqlDataReader reader = command.ExecuteReader();
+
+                products = new List<Products>();
+
+                while(reader.Read()){
+                    products.Add(new Products{
+                        ProductId = int.Parse(reader["id"].ToString()),
+                        Name = reader["product_name"].ToString(),
+                        Price = double.Parse(reader["list_price"].ToString())
+                    });
+                }
+
+                reader.Close();
+            }
+
+            catch(Exception e){
+                Console.WriteLine(e.Message);
+            }
+
+            finally{
+                con.Close();
+            }
+        }
+
+        return products;
+    }
 }
 class Program
 {
@@ -92,5 +175,19 @@ class Program
         {
             Console.WriteLine($"Ürün id : {item.ProductId}, Ürün Ad : {item.Name}, Ürün Fiyat : {item.Price}");
         }
+
+        var product = productdal.GetProductById(3);
+
+        Console.WriteLine($"Product id : {product.ProductId}, Product Name : {product.Name}, Product Price : {product.Price}");
+
+        var new_products = new MySqlProductDal();
+
+        var urunler = new_products.Find("Northwind");
+
+        foreach (var item in urunler)
+        {
+            Console.WriteLine($"ürün ad : {item.Name}, ürün id : {item.ProductId}, ürün fiyat : {item.Price}");
+        }
+
     }
 }
